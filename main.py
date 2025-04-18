bfecaeb282593b313609b986f736aed30e57414f
Kohda-Kaminotsuki <biggsjc@acad.sunybroome.edu>
2025-04-18 14:18:42 -0400

    0.3 Git ignore updated, pygame creates closable black screen and is ready to add events

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
 if __name__ == "__main__":
     main()
