6f1ef6d0e928df09b9d1b5d245427a61ff70f4cb
Kohda-Kaminotsuki <biggsjc@acad.sunybroome.edu>
2025-04-19 12:39:47 -0400

    0.5 Player now appears

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    #screen.fill("black")
+    print(f" {player.radius}")
+    screen.fill("black")
     #player.draw(screen)
     while True == True:
+        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        pygame.display.update()
         clock.tick(60)
-        player.draw(screen)
         dt = clock.tick(60)/1000
-        print(dt)
 if __name__ == "__main__":
     main()
