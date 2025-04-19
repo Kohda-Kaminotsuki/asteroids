59c8b6b6256b54161873e7ae62a71a3912455122
Kohda-Kaminotsuki <biggsjc@acad.sunybroome.edu>
2025-04-19 12:29:01 -0400

    0.4 Code Syntactically clean, no player appearing bug

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pygame
+from circleshape import *
+from player import *
 from constants import *
+dt = 0
+player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    #screen.fill("black")
+    #player.draw(screen)
     while True == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        clock.tick(60)
+        player.draw(screen)
+        dt = clock.tick(60)/1000
+        print(dt)
 if __name__ == "__main__":
     main()
